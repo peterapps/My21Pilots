7f265271fb206638e8d8ac49af81ab4bc3cb06a3
peterapps <peterapps2016@gmail.com>
2018-11-26 20:31:36 -0500

    Fetched lyrics

--- Lyrics/get_lyrics.py
+++ Lyrics/get_lyrics.py
@@ -16,7 +16,6 @@ def get_song_path(artist_name, song_title):
 	data = {'q': song_title}
 	response = requests.get(search_url, params=data, headers=headers)
 	json = response.json()
-	song_info = None
 	for hit in json['response']['hits']:
 		if artist_name in hit['result']['primary_artist']['name']:
 			return hit['result']['api_path']
@@ -38,13 +37,24 @@ def get_lyrics(song_api_path):
 f = open('songs_list.txt', 'r')
 songs = f.read().split('\n')
 f.close()
+songs = [s for s in songs if len(s) > 0]
+not_founds = []
 
-for title in songs:
-	print(title)
+for i in range(len(songs)):
+	title = songs[i]
+	print(str(i + 1) + '/' + str(len(songs)) + '\t' + title)
 	path = get_song_path('twenty one pilots', title)
-	print(path)
-	lyrics = get_lyrics(path)
-	lyrics = re.sub('\[.*\]\n', '', lyrics)
-	f = open('Dataset/' + title + '.txt', 'w')
-	f.write(lyrics)
-	f.close()
+	if path:
+		lyrics = get_lyrics(path)
+		lyrics = re.sub('\[.*\]\n', '', lyrics)
+		f = open('Dataset/' + str(i + 1).zfill(3) + '_ ' + title + '.txt', 'w')
+		f.write(lyrics)
+		f.close()
+	else:
+		print('\t\tNot found.')
+		not_founds.append((i, title))
+
+print('Done.')
+print('The following were not found:')
+for i in not_founds:
+	print('\t{}. {}'.format(i[0], i[1]))
